e3f7e1780e0c1fdeda9f3431f055c88de6c92edb
keagud <keagud@protonmail.com>
2023-01-23 15:55:33 -0800

    update tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,7 +1,8 @@
 import pytest
 
-from della import tasks
-from della import cli_api
+from della.node import Node, TaskNode, RootNode
+from della.shell import Shell
+
 
 @pytest.fixture(params=["yaml", "toml", "json"])
 def get_filepath(request):
@@ -10,24 +11,13 @@ def get_filepath(request):
 
 @pytest.fixture()
 def file_load(get_filepath):
-    return tasks.TaskNode.init_from_file(get_filepath)
+    return RootNode(get_filepath)
 
-
-@pytest.fixture
-def make_manager(get_filepath):
-    return cli_api.TaskManager(get_filepath)
-
+@pytest.fixture()
+def make_shell(get_filepath):
+    return Shell(get_filepath)
 
 def test_actions(file_load):
     root_node = file_load
 
     assert root_node is not None
-    assert root_node.content == "this is the root node"
-
-
-def test_task_manager(get_filepath):
-    task_manager = cli_api.TaskManager(get_filepath)
-
-
-
-
